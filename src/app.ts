8a936f58a12bc2de18bb8a9274e97d64f5bc3a42
SPatil2026 <153353355+SPatil2026@users.noreply.github.com>
2026-03-26 18:25:40 +0530

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ config(); // Loads .env file automatically
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173 ",
+    origin: "https://pickelball-app.vercel.app",
     credentials: true
 }));
 
